1f4fc47b32bdcc5095aeeffd83f119ffdea9fb49
IReturnToNothingness <invithefirst@yandex.ru>
2022-09-08 14:51:23 +0300

    Upload

---
+++ Bakehouse/Bakehouse/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Bakehouse.Models;
+
+namespace Bakehouse.Controllers
+{
+    public class HomeController : Controller
+    {
+        BakeContext db;
+        new Logic logic = new Logic();
+        public HomeController(BakeContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            logic.GetPrice(db.Products.ToList());
+            logic.FutureHour(db.Products.ToList());
+            return View(db.Products.ToList());
+        }
+    }
+}
